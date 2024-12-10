db5f72ca0b68d1621ea9c6e2d95624a6d95a3b6d
WilhelmReyes <wilhelmeselpro@gmail.com>
2024-12-09 18:21:00 -0600

    Updated a hell lot of my portafolio, it has all the info displayed, now i just need styling a little bit.

--- readme.txt
+++ readme.txt
@@ -1,3 +1,3 @@
-En este Readme documentare informacion importante sobre el proyecto
 
-el proyecto utiliza la biblioteca de bootstrap para facilitar el diseño de la pagina web
+
+el proyecto utiliza el framework tailwind para facilitar el diseño de la pagina web
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#cdcffe',
+        'background': '#010428',
+        'primary': '#777efd',
+        'secondary': '#9c0297',
+        'accent': '#fc1db1',
+       },               
+    },
+  },
+  plugins: [],
+};
